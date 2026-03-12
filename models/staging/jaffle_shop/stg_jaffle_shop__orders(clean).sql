6ef280f18ae895e7357760d484da88794b9cd5f8
pdbawse@infocepts.com <pdbawse@infocepts.com>
2026-03-12 07:01:45 +0000

    Last Chapter

--- models/staging/jaffle_shop/stg_jaffle_shop__orders(clean).sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders(clean).sql
@@ -1,7 +1,18 @@
-  select
+with 
+
+source as (
+   select * from {{ source('jaffle_shop', 'orders') }} 
+),
+
+renamed as (
+      select
         id as order_id,
         user_id as customer_id,
         order_date,
         status as order_status
 
     from {{ source('jaffle_shop', 'orders') }}
+
+)
+
+select * from renamed
